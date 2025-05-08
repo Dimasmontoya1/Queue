1eb364d78c9acac5301e6f4fe8c2004a87b526ff
Dimasmontoya1 <116789066+Dimasmontoya1@users.noreply.github.com>
2025-05-08 21:14:19 +0800

    Add files via upload

---
+++ queue.py
@@ -0,0 +1,28 @@
+from dis import show_code
+
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        if not self.is_empty():
+            return self.items.pop(0)
+        else:
+            raise IndexError("Dequeue from empty queue")
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[0]
+        else:
+            return None
+
+    def size(self):
+        return len(self.items)
+    print("Dequeued:", dequeue(0))
+    show_code()
